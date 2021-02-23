d00b44e8bbfd91f9515a91e0e5bb6d27c600d1d6
kanta-ko <kanta080@student.ubc.ca>
2021-02-23 23:52:43 +0800

    commit

---
+++ task2.java
@@ -0,0 +1,10 @@
+public class task2 {
+    public static void main(String[] args){
+        
+        int a = 5;
+        String b = (a % 2 == 0)? "even": "odd";
+        System.out.println(b);
+
+    }
+    
+}
