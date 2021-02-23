ed89ecd3a68a776afe2c49b7ec6451546e09d28f
kanta-ko <kanta080@student.ubc.ca>
2021-02-24 00:00:45 +0800

    main

---
+++ task3a.java
@@ -0,0 +1,13 @@
+public class task3a {
+    public static void main(String[] args){
+        int X = 1;
+        int scale = 1;
+        if (X > 10){
+            int score = scale * 3;
+        }
+        else {
+            int score = scale * 4;
+        }
+    }
+    
+}
