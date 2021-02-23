b7a6c32b17d2a109ead267de9aa18d5fad084c13
kanta-ko <kanta080@student.ubc.ca>
2021-02-23 15:28:10 +0800

    commit

---
+++ task1.java
@@ -0,0 +1,15 @@
+import util.java.Scanner;
+
+public class task1{
+    public static void main(String[] args){
+        Scanner input = new Scanner();
+
+        System.out.println("Enter a year:");
+        int year = input.nextInt();
+
+
+
+
+    }
+
+}
