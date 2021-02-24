6aec26d3f1fa64f8c228a3cf91087530018023ee
kanta-ko <kanta080@student.ubc.ca>
2021-02-24 10:53:42 +0800

    commit

---
+++ task3b.java
@@ -0,0 +1,12 @@
+public class task3b {
+    public static void main(String[] args){
+        int number = 2;
+        if (number % 3 == 0){
+            System.out.println("i");
+        }
+        else {
+            System.out.println("j");
+        }
+    }
+    
+}
